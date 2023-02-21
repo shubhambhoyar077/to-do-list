328481a0175cfacd5814a13d108e184e2782589a
shubham <shubham.bhoyar077@gmail.com>
2023-02-21 18:36:11 +0530

    Create array object of list and imported display list

--- src/index.js
+++ src/index.js
@@ -1,2 +1,32 @@
 import './style.css';
-console.log("Hello world");
+import displayList from './modules/displayList';
+
+let toDoList = [
+  {
+    index: 0,
+    description: "Organize office",
+    completed: false
+  },
+  {
+    index: 1,
+    description: "Pay bills",
+    completed: false
+  },
+  {
+    index: 2,
+    description: "Meet George",
+    completed: false
+  },
+  {
+    index: 3,
+    description: "Buy eggs",
+    completed: false
+  },
+  {
+    index: 4,
+    description: "Read a book",
+    completed: false
+  },
+]
+
+displayList(toDoList);
