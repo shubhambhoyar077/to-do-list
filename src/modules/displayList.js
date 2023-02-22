120263b7aefc3a7d36e8d7c73bbce674396b4f2a
shubham <shubham.bhoyar077@gmail.com>
2023-02-22 13:28:15 +0530

    Updete display list to renderlist

--- src/modules/displayList.js
+++ src/modules/displayList.js
@@ -1,13 +1,21 @@
 const ulList = document.getElementById('list-items');
 
-const displayList = (toDoList) => {
-  toDoList.forEach((element) => {
+
+const renderList = (listItem) => {
     ulList.innerHTML += `
-          <li class="common-padding"><div><input type="checkbox">  ${element.description}</div>
-          <span class="material-symbols-outlined move-item">
-          more_vert
-          </span></li>`;
-  });
+          <li class="common-padding">
+            <input type="checkbox">
+            <div class="view">
+                <label id="label">${listItem.description}</label>
+                <input type="text" id="edit" class="disable">
+            </div>
+            <span id='move' class="material-symbols-outlined move-item">
+              more_vert
+            </span>
+            <span id='delete' class="material-symbols-outlined disable">
+              delete
+            </span>
+          </li>`;
 };
 
-export default displayList;
+export default renderList;
