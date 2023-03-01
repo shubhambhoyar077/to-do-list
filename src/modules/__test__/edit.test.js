36397e92b50c2c2500f9e81c93ddb325a306e330
shubham <shubham.bhoyar077@gmail.com>
2023-03-01 17:04:29 +0530

    Fix Linter error

--- src/modules/__test__/edit.test.js
+++ src/modules/__test__/edit.test.js
@@ -1,25 +1,23 @@
 /**
  * @jest-environment jsdom
  */
-import ToDoList from "../toDoList";
+import ToDoList from '../toDoList';
 
-//Code here
-describe("Edit Items in list", ()=> {
+describe('Edit Items in list', () => {
   test('Edited task textContent should be equal to updated description', () => {
-    document.body.innerHTML =
-    '<div>' +
-    '  <ul id="list-items"></ul>' +
-    '</div>';
-    const testdescription = "test";
+    document.body.innerHTML = '<div>'
+    + '  <ul id="list-items"></ul>'
+    + '</div>';
+    const testdescription = 'test';
     const toDoList = new ToDoList();
     // Add test task
     toDoList.addItem(testdescription);
     const index = document.querySelector('.view').id;
     // Update test task
-    const update_description = "Test is updated";
-    toDoList.updateItem(index, update_description);
-    toDoList.displayList();//Update html.
+    const updateDescription = 'Test is updated';
+    toDoList.updateItem(index, updateDescription);
+    toDoList.displayList();// Update html.
     const description = document.getElementById('label');
-    expect(description.textContent).toBe(update_description);
+    expect(description.textContent).toBe(updateDescription);
   });
 });
--- src/modules/__test__/mark_conpleted.test.js
+++ src/modules/__test__/mark_conpleted.test.js
@@ -1,19 +1,18 @@
 /**
  * @jest-environment jsdom
  */
-import ToDoList from "../toDoList";
+import ToDoList from '../toDoList';
 
-describe("Mark Completed", ()=> {
+describe('Mark Completed', () => {
   test('Completed task set to true', () => {
-    document.body.innerHTML =
-    '<div>' +
-    '  <ul id="list-items"></ul>' +
-    '</div>';
+    document.body.innerHTML = '<div>'
+    + '  <ul id="list-items"></ul>'
+    + '</div>';
     const testObj = {
-      index:0,
-      description:"test",
-      completed:false
-    }
+      index: 0,
+      description: 'test',
+      completed: false,
+    };
     const toDoList = new ToDoList();
     // Add test task
     toDoList.addItem(testObj);
@@ -21,10 +20,9 @@ describe("Mark Completed", ()=> {
     // Mark as Completed
     const completed = true;
     toDoList.markComplited(testObj.index, completed);
-    toDoList.displayList();//Update html.
+    toDoList.displayList();// Update html.
     const checkbox = document.getElementById('checkbox');
 
     expect(checkbox.checked).toBe(completed);
   });
 });
-
