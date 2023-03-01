4081d6ccc325d5fd88caa07a639fd86b90f0ce55
Misikir Delelew <98164891+misikirdelelew@users.noreply.github.com>
2023-03-01 02:44:42 -0800

    updated JS unit testing code

--- src/modules/__test__/add_delete.test.js
+++ src/modules/__test__/add_delete.test.js
@@ -5,5 +5,16 @@ import ToDoList from "../toDoList";
 
 describe("Add and Delete Items in list", ()=> {
   // Code Here
+  test('Add one new item to the list', () => {
+    document.body.innerHTML =
+    '<div>' +
+    '  <ul id="list-items"></ul>' +
+    '</div>';
+    const description = "test";
+    const toDoList = new ToDoList();
+    toDoList.addItem(description);
+    const list = document.querySelectorAll('#list-items li');
+    expect(list).toHaveLength(1);
+  });
 });
 
