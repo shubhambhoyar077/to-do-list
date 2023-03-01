23ff3f4c5580d590bcb659fb75aec73c90b3823f
Misikir Delelew <98164891+misikirdelelew@users.noreply.github.com>
2023-03-01 02:48:38 -0800

    Update edit.test.js

--- src/modules/__test__/edit.test.js
+++ src/modules/__test__/edit.test.js
@@ -4,4 +4,22 @@
 import ToDoList from "../toDoList";
 
 //Code here
-
+describe("Edit Items in list", ()=> {
+  test('Edited task textContent should be equal to updated description', () => {
+    document.body.innerHTML =
+    '<div>' +
+    '  <ul id="list-items"></ul>' +
+    '</div>';
+    const testdescription = "test";
+    const toDoList = new ToDoList();
+    // Add test task
+    toDoList.addItem(testdescription);
+    const index = document.querySelector('.view').id;
+    // Update test task
+    const update_description = "Test is updated";
+    toDoList.updateItem(index, update_description);
+    toDoList.displayList();//Update html.
+    const description = document.getElementById('label');
+    expect(description.textContent).toBe(update_description);
+  });
+});
